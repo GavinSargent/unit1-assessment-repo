7336d7274e262280cedfb8db860f96af6eb2b2ab
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-02-17 11:37:16 -0700

    initial-commit-Unit1-assessment

---
+++ sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+/////////ANSWER/////////
+// Planning
+// Analysis of Requirement
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "Analysis of requirements is the second step. This generally is when the plan is broken down even more by looking at what resources will be needed and gathering said resources. It also includes deciding what requirements the software neeeds to have."
+const design = "Design is deciding what the app is going to look like. This could include themes and visual design. This is also when the language that will be used is decided."
+const implementation = "Implementation is when we actually set into motion the work from the previous three steps. This is when the core coding begins and the software is built."
+const testingAndIntegration = "After the software is built, this is when it is tested. This is hopefully where a lot of the bugs and problems with code are found. This is also where the software is tested on the platform it is intended for, such as a mobile phone. Quality assurance is a main part of this section."
+const maintain = "This is the final step after the software is released. It is when bugs that weren't found in the testing and integration portion are found and fixed. It is also when updates or other theme changes may be added if necessary."
