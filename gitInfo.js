ca5318c1cef817aea8cad9b040a2e823081ad672
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-02-17 12:05:24 -0700

    finished-git-section

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,8 @@
 
 //CODE HERE
 
+let gitDefinition = "A git is version control that allows you to save snapshots of your project"
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition = "GitHub is a website that acts as a cloud service for gits. It is a place where gits can be stored that isn't local allowing a different place to backup projects and share projects with others."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -27,6 +31,8 @@
 
 //CODE HERE
 
+let gitInitDefinition = "git init creates a git repository in the directory one is in."
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -35,6 +41,8 @@
 
 //CODE HERE
 
+let gitCloneDefinition = "git clone creates a copy of a git"
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +50,8 @@
 */
 
 //CODE HERE
+
+let gitStatusDefinition = "git status allows one to check the status of a git to see if it is ready to be added into the repository"
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +64,10 @@
 
 //CODE HERE
 
+let gitAddDefinition = "git add adds a file to the staging area to be ready to commit it to the repository. It can be used to add a specific file or all the files in the directory one is currently in depending on the command"
+
+let gitAddCode = "git add ."
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +79,9 @@
 
 //CODE HERE
 
+let gitCommitDefinition = "git commit commits the git to the repository"
+
+let gitCommitCode = "git commit -m 'initial commit'"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +90,5 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = "git push pushes a copy of your repository to GitHub"
